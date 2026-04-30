1491f84baf22c09f6f06e0306251d86eb78e0b32
virlancristian <rethvol111@gmail.com>
2026-04-30 21:22:38 +0300

    feat: loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+async function sleep() {
+    return new Promise((resolve) => setTimeout(resolve, 3000));
+}
+
+export default async function Home() {
+    await sleep();
     return <h1>AnyTopic</h1>
 }
